3ea9ca7584041e305ccb2fcbb3f4cf5245caad40
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-10-23 11:39:38 +0500

    redesigned comments, fixed bugs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    @commment.user = current_user
+    @comment.user = current_user
 
     if @comment.save
       flash[:notice] = "Comment has been craeted"
@@ -16,12 +16,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = @comment.find(params[:id])
+    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:body)
