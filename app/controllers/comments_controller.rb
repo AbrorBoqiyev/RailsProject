f8d804b91f472da200f8dd083b08d1c0940259b9
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-10-23 08:03:01 +0500

    cleaned some files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,25 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
-  def create; end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @commment.user = current_user
 
-  def destroy; end
+    if @comment.save
+      flash[:notice] = "Comment has been craeted"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Comment has not been ceated"
+      redirect_to post_path(@post)
+    end
+  end
+
+  def destroy
+    @comment = @comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 
   private
 
