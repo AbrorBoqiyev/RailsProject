8e426ac36dd50a47db32302c9a7626db2d4995c7
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-10-28 20:34:50 +0500

    connected rails_learning to psql

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: "Tester", email: 'test@example.com', password: 'password', password_confirmation: 'password')
 
   10.times do |x|
     Post.create(title: "Title #{x}", body: "Body #{x} words go here Idk", user_id: User.first.id)
